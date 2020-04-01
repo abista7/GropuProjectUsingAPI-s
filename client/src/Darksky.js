aba3079f362e20df9ba9b760185ececd529febbb
Arjun <arjunbista428@gmail.com>
2020-04-01 16:46:24 -0700

    update

--- client/src/Darksky.js
+++ client/src/Darksky.js
@@ -13,12 +13,28 @@ class Darksky extends React.Component {
         }
     }
 
+    componentDidMount() {
+        this.callApi()
+          .then(res => this.setState({ response: res.express }))
+          .catch(err => console.log(err));
+      }
+    
+      callApi = async () => {
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+    
+        if (response.status !== 200) throw Error(body.message);
+    
+        return body;
+      };
+
+
+
         check = e => {
-            const req = '/abista7?city=' + this.state.city
+            const req = '/darksky?city=' + this.state.city
             axios.get(req)
-                .then(response => {
-                    alert(response.data.response);
-                })
+                .then(res => this.setState({ result: res.data }))
+            .catch(err => console.log(err));
         }
 
   
